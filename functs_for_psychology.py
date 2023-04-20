b90a69054ef0ebd4710a7b3247b20dc1011e161d
Mak7-kurochka <129383186+Mak7-kurochka@users.noreply.github.com>
2023-04-20 10:38:29 +0200

    Update functs_for_psychology.py

--- functs_for_psychology.py
+++ functs_for_psychology.py
@@ -1,18 +1,30 @@
+#Here i importing all libraries which will use in work
 import pandas as pd
 from matplotlib import pyplot as plt
 from collections import Counter
 
+#Selecting a style for plots
+plt.style.use('fivethirtyeight')
+
+#Reading a table which i want to annalise
 df = pd.read_csv('Zadowolenie z komunikacji miejskiej mieszkańców Krakowa (Responses).csv')
+
+#Dropping two columns beacuse i don't need them
 df.drop(columns=['Timestamp', 'Email Address'], inplace=True)
 
+#Creating a functionsthat will help me write less lines 
 def create_pie(name_file):
+    
+    #I starting analysis by creation a new table with counts of reccuring values
     fstdf = pd.DataFrame(df[name_file].value_counts())
+    #Creating a list which contain all indexes. I'll use them like a legend fro the plot
     age = list(fstdf.index)
 
+    #Creating a variable which will be used as values for the plot
     count = []
+    #
     for i in range(fstdf[name_file].count()):
         count.append(fstdf[name_file][i])
-    for i in range(len(count)): count[i] *= 100
 
     inp = input('Maybe you want add a title for legend table?: ')
     if inp == 'Yes' or inp == 'yes':
@@ -34,20 +46,45 @@ def create_pie(name_file):
 
         plt.title(name_file)
 
-def create_bar_for_str(name_file):
-    my_counter = Counter()
+        plt.show()
 
-    for response in df[name_file]:
-        my_counter.update(response.split(','))
-    smo = []
-    popularity = []
+def create_bar(name_file, inp=None):
+        for rr in df[name_file]:
+            if rr == int or rr == float:
+                break
+            else:
+                break
+        if type(rr) == int or type(rr) == float:
+            for_bar = pd.DataFrame(df[name_file].value_counts())
+            name_f_b = list(for_bar.index)
+            numb = list(for_bar[name_file])
 
-    for item in my_counter.most_common(15):
-        smo.append(item[0])
-        popularity.append(item[1])
-    smo, popularity
+            plt.bar(name_f_b, numb)
 
-    plt.barh(smo, popularity)
-    plt.gca().invert_yaxis()
-    plt.tight_layout()
-    plt.title(name_file)
+            plt.tight_layout()
+
+            plt.title(name_file)
+
+            plt.show()
+
+        else:
+            my_counter = Counter()
+
+            for response in df[name_file]:
+                if type(response) == str:
+                    my_counter.update(response.split(','))
+                smo = []
+                popularity = []
+
+            for item in my_counter.most_common(15):
+                smo.append(item[0])
+                popularity.append(item[1])
+            
+            if inp == 'h':
+                plt.barh(smo, popularity)
+                plt.gca().invert_yaxis()
+            else:
+                plt.bar(smo, popularity)
+            plt.tight_layout()
+            plt.title(name_file)
+            plt.show()
